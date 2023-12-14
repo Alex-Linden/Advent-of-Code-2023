545b5d2b0808e48a25ee9af6830423e6fdd9f8fe
Alex Linden <alextl2004@gmail.com>
2023-12-13 17:20:02 -0800

    stuck on day10 part2, commiting changes before leaving for the day

--- day10.js
+++ day10.js
@@ -5,6 +5,12 @@ let test1 = fs.readFileSync("day10T1.txt").toString().split(/\r?\n/);
 
 let test2 = fs.readFileSync("day10T2.txt").toString().split(/\r?\n/);
 
+let p2test1 = fs.readFileSync("day10p2t1.txt").toString().split(/\r?\n/);
+
+let p2test2 = fs.readFileSync("day10p2t2.txt").toString().split(/\r?\n/);
+
+let p2test3 = fs.readFileSync("day10p2t3.txt").toString().split(/\r?\n/);
+
 const pipeMoves = {
   "|": [[1, 0], [-1, 0]],
   '-': [[0, 1], [0, -1]],
@@ -18,6 +24,8 @@ class PipeGame {
   constructor(map) {
     this.map = map;
     this.start = this.findStart();
+    this.loop = [];
+    this.PolyArea = 0;
   }
 
   findStart() {
@@ -45,61 +53,108 @@ class PipeGame {
       for (let i = 0; i < moves.length; i++) {
         if (sX === pX + moves[i][0] && sY === pY + moves[i][1]) {
           if (i === 1) {
-            validN.push([pX + moves[0][0], pY + moves[0][1]]);
+            validN.push([pX, pY]);
           } else {
-            validN.push([pX + moves[1][0], pY + moves[1][1]]);
+            validN.push([pX, pY]);
           }
         }
       }
     }
+    // console.log(this.map[validN[0][0]][validN[0][1]], this.map[validN[1][0]][validN[1][1]])
     return validN;
   }
   //need to switch to tracking direction rather than previous pos
   nextStep(curPos, prevPos) {
-    console.log(curPos, prevPos);
+    // console.log(curPos, prevPos);
     let [cX, cY] = curPos;
-    let [pX, pY] = prevPos;
+    let prevPosStr = `${prevPos[0]}-${prevPos[1]}`;
     let pipe = this.map[cX][cY];
-    console.log("nextStep pipe", pipe);
+    // console.log("nextStep pipe", pipe);
     for (let n of pipeMoves[pipe]) {
-      if (pipe === "-") {
-        if (pY !== cY + n[1]) {
-          return [cX + n[0], cY + n[1]];
-        }
-      } else if (pipe === "|") {
-        if (pX !== cX + n[0]) {
-          return [cX + n[0], cY + n[1]];
-        }
-      } else if (pX !== cX + n[0] && pY !== cY + n[1]) {
+      // console.log(curPos, prevPosStr, pipe, `${cX + n[0]}-${cY + n[1]}`)
+      if (prevPosStr !== `${cX + n[0]}-${cY + n[1]}`) {
         return [cX + n[0], cY + n[1]];
       }
     }
+  }
+  cordToString(cords) {
+    return `${cords[0]}-${cords[1]}`;
   }
   //TODO: create find total steps, will implement findStartN and nextStep plus counter
   findFarthest() {
     let [cur1, cur2] = this.findStartNeighbors();
     let prev1 = this.start;
-    let prev2 = this.start;
+    // let prev2 = this.start;
     let i = 1;
+    let seen = new Set();
+    seen.add(this.cordToString(prev1));
+    this.loop.push(prev1);
 
-    while (cur1 !== cur2 && cur1 !== this.start) {
+    while (this.cordToString(cur1) !== this.cordToString(this.start)) {
+      if (seen.has(this.cordToString(cur1))) {
+        // console.log("cords seen")
+        break;
+      }
+      seen.add(this.cordToString(cur1));
+      seen.add(this.cordToString(cur2));
+      this.loop.push(cur1);
+      // this.loop.push(cur2);
+      // console.log(seen)
       let next1 = this.nextStep(cur1, prev1);
-      let next2 = this.nextStep(cur2, prev2);
+      // let next2 = this.nextStep(cur2, prev2);
       prev1 = cur1;
       cur1 = next1;
-      prev2 = cur2;
-      cur2 = next2;
+      // prev2 = cur2;
+      // cur2 = next2;
       i++;
     }
-    return i + 1;
+    console.log(this.loop.length);
+    return Math.ceil(i / 2);
   }
 
+  shoelace() {
+    let len = this.loop.length;
+    let sum1 = 0;
+    let sum2 = 0;
+    let area = 0;
+    for (let i = 0; i < len - 1; i++) {
+      sum1 += this.loop[i][0] * this.loop[i + 1][1];
+      sum2 += this.loop[0][0] * this.loop[len - 1][1];
+    }
+    area = Math.abs(sum1 - sum2) / 2;
+    this.PolyArea = area;
+    return area;
+  }
+
+  pick() {
+    return (this.PolyArea - 0.5 * this.loop.length + 1);
+  }
 }
 
 let t1 = new PipeGame(test1);
 let t2 = new PipeGame(test2);
 let puzzle = new PipeGame(contents);
 
+let p2t1 = new PipeGame(p2test1);
+let p2t2 = new PipeGame(p2test2);
+let p2t3 = new PipeGame(p2test3);
+
 console.log(t1.findFarthest());
-// console.log(t2.findFarthest())
-// console.log(puzzle.findFarthest())
+console.log(t2.findFarthest());
+console.log(puzzle.findFarthest());
+
+console.log(p2t1.findFarthest());
+console.log(p2t1.shoelace());
+console.log(p2t1.pick());
+
+// console.log(p2t2.findFarthest());
+// console.log(p2t2.shoelace());
+// console.log(p2t2.pick());
+
+// console.log(p2t3.findFarthest());
+// console.log(p2t3.shoelace());
+// console.log(p2t3.pick());
+
+
+// console.log("pt2", puzzle.shoelace());
+// console.log("pt2 picks", puzzle.pick());
