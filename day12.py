798da1b7ff61f1218747b7dd9bfce74ed936af58
Alex Linden <alextl2004@gmail.com>
2023-12-22 12:24:23 -0800

    solved part 1 and 2, was on the right track but was over complicating things

--- day12.py
+++ day12.py
@@ -1,6 +1,7 @@
+from functools import cache
+
 puzzle_input = open("day12.txt").read().split("\n")
 test = open("day12t1.txt").read().split("\n")
-
 
 
 class Spring:
@@ -12,35 +13,28 @@ class Spring:
         part 1 spring: string map of possible alignment of springs
         part2 list of ints"""
         springs, count = row.split(" ")
-        count = list(eval(count))
+        count = eval(count)
         return [springs, count]
 
-    def find_possible_arrangements(self, spring, count):
-        print(spring, count)
+    @cache
+    def find_possible_arrangements(self, spring, count, continuous=0):
+        # print(spring, count)
         # base case
-        if len(count) == 0 and spring:
-            return 1
-        elif len(count) > 0 and not spring:
-            return 0
+        if not spring:
+            return not count and not continuous
         total = 0
-        i = 0
-        while i < len(spring):
-            if spring[i] == "?" and (i == 0 or spring[i-1] != "#") and spring[i + 1] != "#":
-                j = i + 1
-                while j < len(spring) and j <= i + count[0]:
-                    if spring[j] in "?#":
-                        j += 1
-                    else:
-                        break
-                total += self.find_possible_arrangements(spring[j:], count[1:])
-            elif spring[i] == "#":
-                j = i + 1
-                while j < len(spring) and j <= i + count[0]:
-                    if spring[j] in "?#":
-                        j += 1
-                    else:
-                        break
-                total += self.find_possible_arrangements(spring[j:], count[1:])
+        q = ".#" if spring[0] == "?" else spring[0]
+        for c in q:
+            if c == "#":
+                total += self.find_possible_arrangements(
+                    spring[1:], count, continuous + 1
+                )
+            else:
+                if continuous:
+                    if count and count[0] == continuous:
+                        total += self.find_possible_arrangements(spring[1:], count[1:])
+                else:
+                    total += self.find_possible_arrangements(spring[1:], count)
         return total
 
     def count_each_row(self):
@@ -48,15 +42,26 @@ class Spring:
         for row in self.record:
             spring, count = self.parse_row(row)
             # print(spring, count)
-            count = self.find_possible_arrangements(spring, count)
+            count = self.find_possible_arrangements(spring + ".", count)
             count_per_row.append(count)
         return count_per_row
 
+    def count_each_row_pt2(self):
+        p2_count = 0
+        for row in self.record:
+            spring, count = self.parse_row(row)
+            spring = (spring + "?") * 5
+            # print(spring, count)
+            count = count * 5
+            p2_count += self.find_possible_arrangements(spring[:-1] + ".", count)
+        return p2_count
+
+
 test1 = Spring(test)
+pz = Spring(puzzle_input)
 
-print(test1.count_each_row())
+print(sum(test1.count_each_row()))
+print(sum(pz.count_each_row()))
 
-
-
-
-
+print("part2", test1.count_each_row_pt2())
+print("part2 pz", pz.count_each_row_pt2())
